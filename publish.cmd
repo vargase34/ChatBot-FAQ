c810a5512d1723670b19c5298d95ccdfc5d8ed10
Emmanuel Vargas-Zapata <emmanuel.vargas@lc.cuny.edu>
2020-11-19 18:42:18 -0500

    made a website and a chatbot to learn more about ML and NLP

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\chatbot-emmanuel-app-bot-faq-Web-Deploy.pubxml -p:Password=uiZFij7otLWs9zCp7qCuwy8Xr0CGiZydcXlaFLLdp0BYf0jZYLqlBuCwgYXK
+
